0a06c308a725353a654425f1dd54558e2910ae09
IamShihabPro <mshihab.pro@gmail.com>
2023-08-06 16:22:07 +0600

    jwt

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
@@ -9,6 +11,22 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(express.json())
 app.use(cors())
+
+const verifyJWT = (req, res, next) =>{
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'}) 
+  }
+  // bearer token
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'}) 
+    }
+    req.decoded = decoded
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgdhrpf.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,8 +49,14 @@ async function run() {
     const productCollection = client.db('hexaShop').collection('products')
     const cartCollection = client.db('hexaShop').collection('carts')
 
-    // users related
+    // jwt
+    app.post('/jwt', (req, res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({token})
+    })
 
+    // users related
     app.get('/users', async(req, res)=>{
       result = await usersCollection.find().toArray()
       res.send(result)
@@ -92,12 +116,16 @@ async function run() {
       res.send(result)
      })
 
-     app.get('/carts', async(req, res)=>{
+     app.get('/carts', verifyJWT, async(req, res)=>{
       const email = req.query.email
       // console.log(email);
       if(!email){
         res.send([])
       }
+     const decodedEmail = req.decoded.email
+     if(email !== decodedEmail){
+      return res.status(403).send({error: true, message: 'forbidden access'}) 
+     }
      const query = { email: email}
      const result = await cartCollection.find(query).toArray()
      res.send(result)
