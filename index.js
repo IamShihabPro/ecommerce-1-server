5b0922307bb47c6409c4bba3348c853acb6af0bf
IamShihabPro <mshihab.pro@gmail.com>
2023-12-09 23:45:37 +0600

    server update

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db('hexaShop').collection('users')
     const productCollection = client.db('hexaShop').collection('products')
